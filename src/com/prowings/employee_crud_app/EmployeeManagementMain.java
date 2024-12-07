e604e42c69815a88ef08ddb4f5779ea086758d36
JD <jayprowings@gmail.com>
2024-12-07 11:05:48 +0530

    CRUD completed

--- src/com/prowings/employee_crud_app/EmployeeManagementMain.java
+++ src/com/prowings/employee_crud_app/EmployeeManagementMain.java
@@ -1,5 +1,7 @@
 package com.prowings.employee_crud_app;
 
+import java.util.List;
+
 public class EmployeeManagementMain {
 	
 	public static void main(String[] args) {
@@ -10,8 +12,23 @@ public class EmployeeManagementMain {
 		Employee emp2 = new Employee("Sham", "HR", 32000);
 		Employee emp3 = new Employee("Sachin", "IT", 76000);
 		
-		repository.addNewEmployee(emp1);
+//		repository.addNewEmployee(emp1);
+//		repository.addNewEmployee(emp2);
+//		repository.addNewEmployee(emp3);
 		
+		Employee fetchedEmployee = repository.getEmployeeById(1);
+		System.out.println("Fetched Employee : "+fetchedEmployee);
+		
+		List<Employee> allEmployeeList = repository.getAllEmployees();
+		System.out.println("All Employees : "+ allEmployeeList);
+		
+		repository.deleteEmployeeById(1);	
+		
+		//update employee
+		
+		Employee updatedEmployee = new Employee("Sachin08", "IT", 98000);
+		
+		System.out.println("Updated Employee record : "+repository.updateEmployee(3, updatedEmployee));
 	}
 
 }
--- src/com/prowings/employee_crud_app/EmployeeRepository.java
+++ src/com/prowings/employee_crud_app/EmployeeRepository.java
@@ -3,7 +3,10 @@ package com.prowings.employee_crud_app;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeRepository {
@@ -29,11 +32,53 @@ public class EmployeeRepository {
 
 	// CRUD - R - read
 	public List<Employee> getAllEmployees() {
-		return null;
+		String getEmployeeByIdQuery = "SELECT * FROM employee";
+		List<Employee> allEmployees = new ArrayList<>();
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery(getEmployeeByIdQuery);
+			
+			while(rs.next())
+			{
+				Employee fetchedEmployee = new Employee();
+				fetchedEmployee.setId(rs.getInt(1));
+				fetchedEmployee.setName(rs.getString(2));
+				fetchedEmployee.setDepartment(rs.getString(3));
+				fetchedEmployee.setSalary(rs.getInt(4));
+				allEmployees.add(fetchedEmployee);
+			}
+			System.out.println("Employee record fetched successfully!!!");
+			
+		} catch (SQLException e) {
+			System.out.println("Some error while executing PreparedStmt for getEmployeeById!!" + e.getMessage());
+		}
+		
+		return allEmployees;
 	}
 
 	public Employee getEmployeeById(int id) {
-		return null;
+		
+		String getEmployeeByIdQuery = "SELECT * FROM employee WHERE id =";
+		Employee fetchedEmployee = new Employee();
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery(getEmployeeByIdQuery+id);
+			
+			while(rs.next())
+			{
+				fetchedEmployee.setId(rs.getInt(1));
+				fetchedEmployee.setName(rs.getString(2));
+				fetchedEmployee.setDepartment(rs.getString(3));
+				fetchedEmployee.setSalary(rs.getInt(4));
+
+			}
+			System.out.println("Employee record fetched successfully!!!");
+			
+		} catch (SQLException e) {
+			System.out.println("Some error while executing PreparedStmt for getEmployeeById!!" + e.getMessage());
+		}
+		
+		return fetchedEmployee;
 	}
 
 	// CRUD - C - create
@@ -66,12 +111,58 @@ public class EmployeeRepository {
 
 	// CRUD - U - update
 	public Employee updateEmployee(int id, Employee updatedEmployee) {
-		return null;
+
+		String updateEmployeeQuery = "UPDATE employee SET name = ?, department = ?, salary = ? WHERE id = ?";
+	    Employee updatedRecord = null;
+	    Employee updatedEmployeeFromDB = null;
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(updateEmployeeQuery);
+			
+			preparedStatement.setString(1, updatedEmployee.getName());
+			preparedStatement.setString(2, updatedEmployee.getDepartment());
+			preparedStatement.setInt(3, updatedEmployee.getSalary());
+			preparedStatement.setInt(4, id);
+			
+			int res = preparedStatement.executeUpdate();
+			
+			if (res>0) {
+				System.out.println("Employee record updated successfully!!");
+			}
+			else
+				System.out.println("Employee record not updated!!");
+			
+			updatedEmployeeFromDB = getEmployeeById(id);
+
+		} catch (SQLException e) {
+			System.out.println("Some error while deleting employee!!" + e.getMessage());
+		}
+		return updatedEmployeeFromDB;
+
 	}
 
 	// CRUD - U - update
 	public boolean deleteEmployeeById(int id) {
-		return true;
+		
+		String deleteEmployeeQuery = "DELETE FROM employee WHERE id = ?";
+		boolean result = false;
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(deleteEmployeeQuery);
+			preparedStatement.setInt(1, id);
+			
+			int res = preparedStatement.executeUpdate();
+			
+			if (res>0) {
+				System.out.println("Employee record deleted from DB successfully!!");
+				result = true;
+			}
+			else
+				System.out.println("Employee record not deleted from DB!!");
+
+		} catch (SQLException e) {
+			System.out.println("Some error while deleting employee!!" + e.getMessage());
+		}
+		return result;
+
 	}
 
 }
